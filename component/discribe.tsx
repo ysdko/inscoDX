10e3c08b9bd9b2297bc623999e652253f44092fb
shinji1095 <shinji1095nameko@gmail.com>
2021-08-19 12:59:42 +0900

    add: reportコンポーネントの追加

--- component/discribe.tsx
+++ component/discribe.tsx
@@ -4,32 +4,32 @@ import hosino from "../public/hosino-50.jpg"
 
 const Discribe = () => {
     return (
-        <div className="card box">
+        <div className="card box box-bottom">
             <div className="card-header">
                 <div className="card-body text-center p-0">
                     <p className="text-center text-underline m-0">リライト</p>
                     <p className="text-center text-underline m-0">ASIAN KUNGFU GENERATION</p>
                 </div>
-                <div className="card-body">
-                    <div className="row">
-                        <div className="col-6">
-                            <Image src={hosino}></Image>
+            </div>
+
+            <div className="card-body">
+                <div className="row">
+                    <div className="col-6">
+                        <Image src={hosino}></Image>
+                    </div>
+                    <div className="col-6 d-flex flex-column text-center">
+                        <div className="text-center mb-2">星野　ひなた</div>
+                        <div className="row justify-content-between">
+                            <div className="col">前回</div>
+                            <div className="col">--.--点</div>
                         </div>
-                        <div className="col-6 d-flex flex-column text-center">
-                            <div className="text-center mb-2">星野　ひなた</div>
-                            <div className="row justify-content-between">
-                                <div className="col">前回</div>
-                                <div className="col">--.--点</div>
-                            </div>
-                            <div className="row justify-content-between">
-                                <div className="col">前回</div>
-                                <div className="col">--.--点</div>
-                            </div>
+                        <div className="row justify-content-between">
+                            <div className="col">前回</div>
+                            <div className="col">--.--点</div>
                         </div>
                     </div>
                 </div>
             </div>
-            
         </div>
     )
 }
--- component/report.tsx
+++ component/report.tsx
@@ -2,8 +2,15 @@ import React from 'react'
 
 const Report = () => {
     return (
-        <div className="box-bottom">
-            Report
+        <div className="card box box-bottom">
+            <div className="card-header">
+                <p className="text-underline">分析レポート</p>
+            </div>
+            <div className="card-body text-center">
+                <p>並外れた歌唱力ですね。</p>
+                <p>音程のズレも</p>
+                <p>ほとんどありません。</p>
+            </div>
         </div>
     )
 }
