12c4972c8fea3c2959673dec436a3d0516592760
shinji1095 <shinji1095nameko@gmail.com>
2021-08-19 11:37:35 +0900

    add: chartコンポーネントの追加

--- component/chart.tsx
+++ component/chart.tsx
@@ -1,9 +1,51 @@
 import React from 'react'
+import  {Radar}  from "react-chartjs-2";
 
 const Chart = () => {
+    const RadarData = {
+        labels: ["Finger Strength", "Power", "Endurance", "Stability", "Flexability"],
+        datasets: [
+          {
+            label: "March",
+            backgroundColor: "rgba(34, 202, 236, .2)",
+            borderColor: "rgba(34, 202, 236, 1)",
+            pointBackgroundColor: "rgba(34, 202, 236, 1)",
+            poingBorderColor: "#fff",
+            pointHoverBackgroundColor: "#fff",
+            pointHoverBorderColor: "rgba(34, 202, 236, 1)",
+            data: [13, 10, 12, 6, 5]
+          }
+        ]
+      };
+      const RadarOptions = {
+        maintainAspectRatio: false,
+        responsive: true,
+        scale: {
+          ticks: {
+            min: 0,
+            max: 16,
+            stepSize: 2,
+            showLabelBackdrop: false,
+            backdropColor: "rgba(203, 197, 11, 1)"
+          },
+          angleLines: {
+            color: "rgba(255, 255, 255, .3)",
+            lineWidth: 1
+          },
+          gridLines: {
+            color: "rgba(255, 255, 255, .3)",
+            circular: true
+          }
+        }
+      };
+      
     return (
         <div>
-            Chart
+            <Radar 
+                data={RadarData}   
+                options={RadarOptions} 
+                height={300}
+            />
         </div>
     )
 }
